e6bf882c4cd5c9a368db4d6f496b6e090e18b848
zheng <tonys91168@gmail.com>
2021-06-12 09:31:04 +0800

    main

--- kangaBot.py
+++ kangaBot.py
@@ -1,5 +1,10 @@
 import discord
+from discord import file
 from discord.ext import commands
+import json
+
+with open('setting.json',mode='r',encoding='utf8')as jfile:
+    jdata = json.load(jfile)
 
 
 bot = commands.Bot(command_prefix='.')
@@ -9,7 +14,12 @@ async def on_raedy():
     print(">> Bot is online <<")
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send()
+async def 圖片(ctx):
+    pic = discord.File(jdata['pic'])
+    await ctx.send(file=pic)
 
-bot.run('ODQ5ODY3NDUyMjUwNzE4MjM5.YLhavg.S23ArrW4i-AocX8L78Tsx4MXoxw')
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)} ms')
+
+bot.run(jdata['TOKEN'])
