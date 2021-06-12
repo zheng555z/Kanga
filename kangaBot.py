ab78798334e1a09d92d495740777a65a85f31847
zheng <tonys91168@gmail.com>
2021-06-12 09:02:59 +0800

    main

--- kangaBot.py
+++ kangaBot.py
@@ -2,10 +2,14 @@ import discord
 from discord.ext import commands
 
 
-bot = commands.Bot(command_prefix='[')
+bot = commands.Bot(command_prefix='.')
 
 @bot.event
 async def on_raedy():
     print(">> Bot is online <<")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send()
 
 bot.run('ODQ5ODY3NDUyMjUwNzE4MjM5.YLhavg.S23ArrW4i-AocX8L78Tsx4MXoxw')
